97ee8879c111ca853539854aa471f701547d0559
Ritesh078bct <078bct068@student.ioepc.edu.np>
2024-05-11 13:50:16 +0545

    cart count added

--- script/amazon.js
+++ script/amazon.js
@@ -78,7 +78,14 @@ document.querySelectorAll('#add-to-cart')
             
     })
 }
-console.log(cart)
+let cartQuantity=0;
+cart.forEach((item)=>{
+    cartQuantity+=item.quantity;
+})
+
+document.querySelector('#cart-quantity').innerHTML=cartQuantity;
+// console.log(cartQuantity);
+// console.log(cart)
 
  })
 })
